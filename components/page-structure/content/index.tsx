a743ffb754ad5017d5e50bfc811a8ba7c8ce5ba0
alinelopesm <alinealopesm@gmail.com>
2024-06-05 18:42:42 -0300

    feat: adiciona seção de informações

--- components/page-structure/content/index.tsx
+++ components/page-structure/content/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Grid } from '@mui/material';
 import { Box } from '@mui/material';
+import About from '@/pages/about';
 
 const ContentPage: React.FC = () => {
   return (
     <Box sx={{ position: 'relative'}}>
       <Grid>
         <Grid item xs={12}>
-          Content
+          <About />
         </Grid>
       </Grid>
     </Box>
---
+++ pages/about.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+
+const About = () => {
+  return (
+    <div id="about">
+      <Typography variant="h4" component="h2" gutterBottom>
+        Informações da Festa
+      </Typography>
+    </div>
+  );
+};
+
+export default About;
