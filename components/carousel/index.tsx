1049116ed922846cae88ff4d691d636d3e07f854
alinelopesm <alinealopesm@gmail.com>
2024-06-06 19:23:51 -0300

    feat: add novas imagens e cria composition

--- components/carousel/index.tsx
+++ components/carousel/index.tsx
@@ -1,35 +1,30 @@
-// components/Carousel.tsx
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import styles from './styles.module.css';
 import Image from 'next/image';
-
-const images = [
-  { src: '/images/pamonha.jpg', alt: 'Pamonha' },
-  { src: '/images/canjica.jpg', alt: 'Canjica' },
-  { src: '/images/curau.jpg', alt: 'Curau' },
-];
+import images from '@/compositions/carousel';
 
 const CustomCarousel: React.FC = () => {
   return (
-    <div className="max-w-md mx-auto my-8">
+    <div id='custom-carousel' className="w-full mx-auto my-8">
       <Carousel
-        showArrows={true}
-        showStatus={false}
-        infiniteLoop={true}
-        autoPlay={true}
+        showArrows
+        showStatus
+        infiniteLoop
+        autoPlay
+        showThumbs
+        swipeable
         interval={3000}
-        showThumbs={false}
         className={styles.carousel}
       >
         {images.map((image, index) => (
           <div key={index}>
-            <Image src={image.src} alt={image.alt} layout="responsive" width={500} height={300} className="rounded-lg" />
+            <Image src={image.src} alt={image.alt} layout="responsive" width={500} height={300} className="rounded-xlg" />
           </div>
         ))}
       </Carousel>
-    </div>
+  </div>
   );
 };
 
