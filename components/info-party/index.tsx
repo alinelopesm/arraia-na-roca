6e9fbb96a5870b160d0830bdc99d1085b80a8c72
alinelopesm <alinealopesm@gmail.com>
2024-06-06 14:45:34 -0300

    feat: cria componte de infos da festa

---
+++ components/info-party/index.tsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import Typography from '@mui/material/Typography';
+import styles from './index.module.css';
+import { useMediaQuery, useTheme } from '@mui/material';
+
+const TextArraia = () => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+
+  return (
+    <div>
+      {!isMobile &&
+        <header className={styles.header}>
+          <Typography variant="body1" component="p">
+            🎉 Venha participar do nosso tradicional Arraiá da Roça no dia <strong>29 de junho de 2024</strong>, um sábado que promete muita diversão e alegria para toda a família!
+          </Typography>
+        </header>
+      }
+
+      <article className={styles.article}>
+        <Typography variant="body1" component="p">
+          🕢 <strong>Data e Horário:</strong> 29 de junho de 2024 - A partir das 18h
+        </Typography>
+      </article>
+
+      {!isMobile &&
+        <article className={styles.article}>
+          <Typography variant="body1" component="p">
+            🌽 <strong>Comidas Típicas</strong>
+          </Typography>
+          <ul className={styles.ul}>
+            <li>Milho verde</li>
+            <li>Pipoca</li>
+            <li>Canjica</li>
+            <li>Paçoca</li>
+            <li>Quentão</li>
+            <li>E muito mais!</li>
+          </ul>
+        </article>
+      }
+
+      <article className={styles.article}>
+        <Typography variant="body1" component="p">
+          🎶 <strong>Atração Musical</strong>
+        </Typography>
+        <Typography variant="body1" component="p">
+          Para animar a noite, contaremos com a presença incrível banda <strong>Joe's Pub</strong>, com um repertório pra lá de especial!
+        </Typography>
+      </article>
+
+      <article className={styles.article}>
+        <Typography variant="body1" component="p">
+          👒 <strong>Traje Típico</strong>
+        </Typography>
+        <Typography variant="body1" component="p">
+          Vista sua melhor roupa caipira e entre no clima da festa! Haverá prêmios para os trajes mais criativos e autênticos.
+        </Typography>
+      </article>
+
+      <article className={styles.article}>
+        <Typography variant="body1" component="p">
+          🚨 <strong>Ingressos:</strong>
+        </Typography>
+        <Typography variant="body1" component="p">
+          Garanta já o seu ingresso! Eles estão à venda no <a>link aqui</a> na pagina.
+          O ingresso garantes todas as atrações acima, mas <strong className="text-yellow-600">não esqueça de levar suas bebibas prediletas</strong><br/>
+          Os ingressos são limitados, então corra e não fique de fora!
+        </Typography>
+      </article>
+    </div>
+  );
+};
+
+export default TextArraia;
